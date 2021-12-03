58636e17c93d4c0b26b40e4de4799e615094b65e
Pitt Wu <kgb00128@gmail.com>
2021-12-03 17:44:20 +0800

    feat: add leaflet map:

--- src/components/IPInfo.vue
+++ src/components/IPInfo.vue
@@ -1,9 +1,31 @@
 <template lang="pug">
-div.absolute.-bottom-64.flex.flex-col.bg-white.gap-y-4.gap-x-12.px-8.pt-8.pb-12.rounded-md(class="lg:-bottom-20 lg:flex-row") info
+.absolute.-bottom-64.flex.flex-col.bg-white.gap-y-4.gap-x-12.px-8.pt-8.pb-12.rounded-md(
+  class="lg:-bottom-20 lg:flex-row"
+)
+  .flex.flex-col(v-for="(item, index) in list")
+    h3.text-xs.mb-2.uppercase {{ item.name }}
+    span.text-2xl 8.8.8.8
 </template>
 
 <script>
+import { reactive, toRefs } from 'vue';
+
 export default {
-  name: 'IPinfo'
+  name: 'IPinfo',
+
+  setup() {
+    const state = reactive({
+      list: [
+        { name: 'IP Address' },
+        { name: 'Location' },
+        { name: 'Timezone' },
+        { name: 'ISP' }
+      ]
+    })
+
+    return {
+      ...toRefs(state)
+    }
+  }
 }
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
 .flex.flex-col.h-screen.max-h-screen
-  .flex.justify-center.relative.bg-home-pattern.bg-cover.px-4.pt-8.pb-32
+  header.z-2.flex.justify-center.relative.bg-home-pattern.bg-cover.px-4.pt-8.pb-32
     .w-full.max-w-screen-sm
       h1.text-white.text-center.text-3xl.pb-4 IP Address Tracker
       .flex
@@ -12,15 +12,34 @@
         .flex.items-center.px-4.cursor-pointer.bg-black.rounded-tr-md.rounded-br-md
           i.w-4.h-4.bg-icon-arrow.bg-center.bg-contain.bg-no-repeat
     IPInfo
+  main#mapid.h-full.z-3
 </template>
 
 <script>
 import IPInfo from '@/components/IPInfo.vue'
+import leaflet from 'leaflet'
+import { onMounted } from 'vue'
 
 export default {
   name: 'Home',
   components: {
     IPInfo
+  },
+
+  setup() {
+    onMounted(() => {
+      const myMap = leaflet.map("mapid").setView([51.505, -0.09], 13);
+      const accessToken = process.env.VUE_APP_MAPBOX_TOKEN
+
+      leaflet.tileLayer(`https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=${accessToken}`, {
+          attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+          maxZoom: 18,
+          id: 'mapbox/streets-v11',
+          tileSize: 512,
+          zoomOffset: -1,
+          accessToken: `${accessToken}`
+      }).addTo(myMap);
+    })
   }
 }
 </script>
